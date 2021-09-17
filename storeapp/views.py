cf90e7a0c974a727063f1e550c799a539344e6fc
rutvik2611 <rutvik2611@gmail.com>
2021-09-17 10:17:51 -0400

    Update:Viewset(Users and Book)

--- storeapp/views.py
+++ storeapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from storeapp import models, serializers
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+        This to manage user 
+        creation, list, update and delete
+    """
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+        This to manage books 
+        creation, list, update and delete
+    """
+    queryset = models.Books.objects.all()
+    serializer_class = serializers.BookSerializer
