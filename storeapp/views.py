6e1d37c5ed796c540bb9c01dbf0831fe9d69876f
rutvik2611 <rutvik2611@gmail.com>
2021-09-17 12:20:39 -0400

    Fix:Minor Bug

--- storeapp/views.py
+++ storeapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 from storeapp import models, serializers
 
