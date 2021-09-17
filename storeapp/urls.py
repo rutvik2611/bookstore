81317525c67fefe71570a9933301adbc93a033da
rutvik2611 <rutvik2611@gmail.com>
2021-09-17 10:17:22 -0400

    Add:URL file for storeapp (users and book viewset)

--- storeapp/urls.py
+++ storeapp/urls.py
@@ -3,5 +3,9 @@ from storeapp import views
 
 
 urlpatterns = [
-
+    path('users/', views.UserViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('users/<int:pk>/', views.UserViewSet.as_view({'delete': 'destroy', 'get' : 'retrieve', 'put': 'update'})),
+    
+    path('books/', views.BookViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('books/<int:pk>/', views.BookViewSet.as_view({'delete': 'destroy', 'get' : 'retrieve', 'put': 'update'})),
 ]
